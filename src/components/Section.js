7f40ca9ca8b30515644eec1c76e9c01af9d9730b
Anouk Rimola <anouk138@gmail.com>
2021-08-17 11:19:06 -0600

    update

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,23 +2,19 @@
 
 function Section() {
     return(
-        <div className="card mb-3 py-5" style={{maxWidth: "100rem"}} >
-         <div className="row no-gutters">
-          <div className="col-md-4">
-           <img src={"https://www.fundacionctic.org/sites/default/files/styles/portfolio_square/public/2018-06/bigdata.jpg?itok=vNWY-hmx"} alt="tec" />
+        <div className="container-fluid my-5">
+          <div className="row"> 
+            <div className="col">
+            <img src={"https://www.bneural.io/blog/wp-content/uploads/2019/09/blog10-700x600.jpg"} alt="tec" />
+            </div>
+            <div className="col pr-3">
+              <h1>Tecnología</h1>
+              <p >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt dui a nulla aliquet, dignissim condimentum felis posuere. Duis ut velit non sem congue sodales. Suspendisse potenti. Nam in tortor nec lorem varius tempor. Suspendisse hendrerit neque in sem gravida dictum. Pellentesque dignissim nunc eu lorem convallis, ut feugiat tellus pharetra. Aliquam ut purus non quam sagittis auctor cursus ut eros. Ut quis felis tortor.</p>
+              <p>
+              Morbi hendrerit leo ut varius pellentesque. Donec non massa velit. Praesent bibendum risus at orci finibus tincidunt. Etiam tincidunt, ligula in varius finibus, lacus urna vestibulum ex, in accumsan quam lacus eu justo. Mauris tristique nisl sit amet iaculis volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur porttitor, tortor nec eleifend vehicula, lectus orci imperdiet turpis, volutpat porttitor ante metus sed arcu. Cras faucibus ultricies enim, ut mollis ante bibendum a. Proin vulputate gravida mi eu pretium. Sed eget rutrum tellus, non ultricies ipsum. </p>
+            </div>
+          </div>
         </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title px-5">Card title</h5>
-        <p className="card-text px-5">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt dui a nulla aliquet, dignissim condimentum felis posuere. Duis ut velit non sem congue sodales. Suspendisse potenti. Nam in tortor nec lorem varius tempor. Suspendisse hendrerit neque in sem gravida dictum. Pellentesque dignissim nunc eu lorem convallis, ut feugiat tellus pharetra. Aliquam ut purus non quam sagittis auctor cursus ut eros. Ut quis felis tortor.
-
-        Morbi hendrerit leo ut varius pellentesque. Donec non massa velit. Praesent bibendum risus at orci finibus tincidunt. Etiam tincidunt, ligula in varius finibus, lacus urna vestibulum ex, in accumsan quam lacus eu justo. Mauris tristique nisl sit amet iaculis volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur porttitor, tortor nec eleifend vehicula, lectus orci imperdiet turpis, volutpat porttitor ante metus sed arcu. Cras faucibus ultricies enim, ut mollis ante bibendum a. Proin vulputate gravida mi eu pretium. Sed eget rutrum tellus, non ultricies ipsum. </p>
-        <p className="card-text px-5">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt dui a nulla aliquet, dignissim condimentum felis posuere. Duis ut velit non sem congue sodales. Suspendisse potenti. Nam in tortor nec lorem varius tempor. Suspendisse hendrerit neque in sem gravida dictum. Pellentesque dignissim nunc eu lorem convallis, ut feugiat tellus pharetra. Aliquam ut purus non quam sagittis auctor cursus ut eros. Ut quis felis tortor.</p>
-        <p className="card-text"></p>
-      </div>
-    </div>
-  </div>
-</div>
     )
 }
 
